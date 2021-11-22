295d58613100fd50895d6a03377af6a3d9fe23ed
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-11-22 16:16:58 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Cleyson Silva on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
